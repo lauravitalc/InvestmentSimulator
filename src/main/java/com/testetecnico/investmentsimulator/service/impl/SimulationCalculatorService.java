c191724a1e10f8eba2f0fe9742c4ab10e5bbb016
lauravitalc <laura_vitalc@hotmail.com>
2026-06-18 12:07:45 -0300

    feat: create new private methods and enhance readability

--- src/main/java/com/testetecnico/investmentsimulator/service/impl/SimulationCalculatorService.java
+++ src/main/java/com/testetecnico/investmentsimulator/service/impl/SimulationCalculatorService.java
@@ -11,23 +11,11 @@ import java.math.RoundingMode;
 @Component
 public class SimulationCalculatorService implements SimulationCalculator {
     public SimulationResult calculate(InvestmentSimulation simulation) {
-        BigDecimal monthlyRate = simulation.getAnnualRate()
-                .divide(BigDecimal.valueOf(100), 10, RoundingMode.HALF_UP)
-                .divide(BigDecimal.valueOf(12), 10, RoundingMode.HALF_UP);
-
-        BigDecimal initial = simulation.getInitialContribution() != null
-                ? simulation.getInitialContribution()
-                : BigDecimal.ZERO;
+        BigDecimal monthlyRate = calculateMonthlyRate(simulation);
+        BigDecimal initial = calculateInitialContribution(simulation);
 
         BigDecimal finalValue = initial;
-
-        for (int month = 1; month <= simulation.getTimeMonths(); month++) {
-            finalValue = finalValue
-                    .multiply(BigDecimal.ONE.add(monthlyRate))
-                    .add(simulation.getMonthlyContribution());
-        }
-
-        finalValue = finalValue.setScale(2, RoundingMode.HALF_UP);
+        finalValue = calculateFinalValue(simulation, finalValue, monthlyRate);
 
         BigDecimal totalInvested = initial
                 .add(simulation.getMonthlyContribution()
@@ -37,6 +25,34 @@ public class SimulationCalculatorService implements SimulationCalculator {
         BigDecimal estimatedTax = totalReturn.multiply(taxRate(simulation.getTimeMonths()));
         BigDecimal netValue = finalValue.subtract(estimatedTax).setScale(2, RoundingMode.HALF_UP);
 
+        return buildResult(simulation, finalValue, totalInvested, totalReturn, estimatedTax, netValue);
+    }
+
+    private BigDecimal calculateMonthlyRate(InvestmentSimulation simulation) {
+        return simulation.getAnnualRate()
+                .divide(BigDecimal.valueOf(100), 10, RoundingMode.HALF_UP)
+                .divide(BigDecimal.valueOf(12), 10, RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal calculateInitialContribution(InvestmentSimulation simulation) {
+        return simulation.getInitialContribution() != null
+                ? simulation.getInitialContribution()
+                : BigDecimal.ZERO;
+    }
+
+    private BigDecimal calculateFinalValue(InvestmentSimulation simulation, BigDecimal finalValue,BigDecimal monthlyRate) {
+        for (int month = 1; month <= simulation.getTimeMonths(); month++) {
+            finalValue = finalValue
+                    .multiply(BigDecimal.ONE.add(monthlyRate))
+                    .add(simulation.getMonthlyContribution());
+        }
+
+        finalValue = finalValue.setScale(2, RoundingMode.HALF_UP);
+
+        return finalValue;
+    }
+
+    private SimulationResult buildResult(InvestmentSimulation simulation, BigDecimal finalValue, BigDecimal totalInvested, BigDecimal totalReturn, BigDecimal estimatedTax, BigDecimal netValue) {
         var result = new SimulationResult();
         result.setSimulation(simulation);
         result.setFinalValue(finalValue);
